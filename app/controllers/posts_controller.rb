9784984a046407ce7aeecf9c3092a2934ca4e90a
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-11-10 23:22:13 -0500

    edit and delete functionality for posts, full CRUD completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @comments.each do |comment|
+      comment.destroy
+    end
+    @post.destroy
+    redirect_to posts_path
+  end
 
   private
   def post_params
